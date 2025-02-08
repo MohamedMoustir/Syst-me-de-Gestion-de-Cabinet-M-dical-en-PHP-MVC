64089d81f6aebfb054f7b43f38465f4400397cb2
MohamedMoustir <itsmoustir@gmail.com>
2025-02-08 02:35:57 +0100

    Ajout du répertoire public

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../core/Router.php';
-
 use Core\Router;
-
 $router = new Router();
 $router->dispatch();
+
+// require('../app/View/auth/register.php');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
